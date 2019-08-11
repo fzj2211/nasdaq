3fa7be3bf91004d65716d87fc56d368259c68b3f
fzj2211 <862554245@qq.com>
2019-08-12 00:02:16 +0900

    feat:add build folder

---
+++ build/precache-manifest.1eae59a843f3ccf97d039a19cf2920d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2656cac814d77aa02023327007f05ed9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c27fe75ee37b6ba199e8",
+    "url": "/static/css/main.d5c40be2.chunk.css"
+  },
+  {
+    "revision": "a10907e982ba49e8a5ca",
+    "url": "/static/js/2.3715de9b.chunk.js"
+  },
+  {
+    "revision": "c27fe75ee37b6ba199e8",
+    "url": "/static/js/main.251407dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
